e255940d579603f5ce37126cf0d3727f892de202
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@accenture.com>
2018-01-31 11:03:07 +0530

    added chane

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -3,7 +3,7 @@ package org.ravi.helloworld;
 public class Hello {
 	
 	public String displayHelloMessage(String message){
-                   public int j;
+                 int j;
 		message="hi";
            // System.out.println("test log");
 		return message;
